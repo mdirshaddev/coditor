2aa20bd87a4e5fa73361ffca90131cb584162e0a
Md Irshad <mdirshaddev+github@gmail.com>
2023-03-25 12:19:41 +0530

    chore: update for landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
-      <h1>Next.js Page</h1>
+      <h1 className='select-none cursor-grab hover:underline'>
+        My personal monorepo based website
+      </h1>
     </div>
   );
 }
