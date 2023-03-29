29964b7a9b2b4312342e3d617df65a98a0e1425f
Md Irshad <mdirshaddev+github@gmail.com>
2023-03-29 23:39:32 +0530

    chore: ignore for eslint, next env file and bash script for version

---
+++ apps/web/scripts/get-version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Version key/value should be on his own line
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+echo $PACKAGE_VERSION
